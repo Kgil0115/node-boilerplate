25efc35b3dfe4c417b1b3f239dc3e691aaa625d4
kgil0115 <gilwon0115@gmail.com>
2021-02-13 22:58:47 +0900

    first commit. update

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: 1
+    },
+    password: {
+        type: String,
+        maxlength: 50
+    },
+    role: {
+        type: Number, // 1 이면 관리자, 0이면 일반유저
+        default: 0
+    },
+    image: String,
+    token: {
+        type: String
+    },
+    tokenExp: {
+        type: Number
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports= {User}
